e1ec8a131648e2e597cd84ab70ecc1b078f8ffb5
AleksGrisins <aleks.grisins@magebit.com>
2024-07-10 10:59:25 +0300

    Solution to the task

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
-function test() {
-  alert('GOTCHA');
-  window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
+// accept button
+function accept() {
+  console.log('hooray');
+  alert('Success');
+  hideForm();
 }
+
+// reject button
+function reject() {
+  console.log('fail');
+  alert('Rejected');
+  hideForm();
+}
+
+// simple hide
+// document.getElementById('container').style.display = 'none';
+
+// hide form function
+function hideForm() {
+  let x = document.getElementById("container");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.classList.add('hidden');
+    setTimeout(function() {
+      x.style.display = 'none';
+    }, 300);
+  }
+}
+
+// close button
+document.getElementById('closeBtn').addEventListener('click', function() {
+  let container = document.querySelector('.container');
+  container.classList.add('hidden');
+  hideForm();
+});
